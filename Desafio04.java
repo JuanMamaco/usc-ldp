75046c6be207e8ac5591c7f5b8c53c5bb1d282b9
Juan Peres <70271973+JuanMamaco@users.noreply.github.com>
2024-03-22 19:52:16 -0300

    Create Desafio04.java

---
+++ Desafio04.java
@@ -0,0 +1,55 @@
+package Juan;
+import java.util.Scanner;
+
+public class Desafio04 {
+	
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite um número de 1 a 7: ");
+		int numero = scanner.nextInt();
+		
+		String diaSemana;
+		
+		switch (numero) {
+			
+			case 1:
+				diaSemana = "Domingo";
+				break;
+
+			case 2:
+				diaSemana = "Segunda-feira";
+				break;
+				
+			case 3:
+				diaSemana = "Terça-feira";
+				break;
+				
+			case 4:
+				diaSemana = "Quarta-feira";
+				break;
+				
+			case 5:
+				diaSemana = "Quinta-feira";
+				break;
+				
+			case 6:
+				diaSemana = "Sexta-feira";
+				break;
+				
+			case 7:
+				diaSemana = "Sábado";
+				break;
+				
+			default:
+				diaSemana = "Dia Inválido";
+				break;
+				
+		}
+				
+				System.out.println("O dia da semana que corresponde ao número " + numero + " é " + diaSemana);
+				scanner.close();
+				
+			}
+}
