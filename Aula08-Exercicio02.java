c440e96cba9ca72886516c61217c0ddf9ba1d778
Juan Peres <70271973+JuanMamaco@users.noreply.github.com>
2024-04-04 23:38:12 -0300

    Update Aula08-Exercicio02.java

--- Aula08-Exercicio02.java
+++ Aula08-Exercicio02.java
@@ -1,8 +1,8 @@
-package Exercicio02;
+package Aula8;
 
 import java.util.Scanner;
 
-public class Exercicio2 {
+public class Exercicio02 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
