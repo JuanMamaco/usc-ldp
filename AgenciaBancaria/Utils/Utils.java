1b3867e2417b78d2a340e1d1f3a600e34d9c3a05
Juan Peres <70271973+JuanMamaco@users.noreply.github.com>
2024-05-16 18:13:12 -0300

    Update Utils.java

--- AgenciaBancaria/Utils/Utils.java
+++ AgenciaBancaria/Utils/Utils.java
@@ -1,20 +1,19 @@
-package Utils;
+package utils;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.text.SimpleDateFormat; 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Utils {
-	
-	static NumberFormat FormatandoNumeros = new DecimalFormat("R$ #,##0.00"); 
-	static SimpleDateFormat FormatandoData = new SimpleDateFormat("dd/MM/yyyy"); 
+    static NumberFormat formatandoNumeros = new DecimalFormat("R$,#,##0.00");
+    static SimpleDateFormat formatandoData = new SimpleDateFormat("dd/MM/yyyy");
 
-	public static String dateToString(Date data) {
-		return Utils.FormatandoData.format(data);
-	}
+    public static String dateToString(Date data) {
+        return Utils.formatandoData.format(data);
+    }
 
-	public static String doubleToString(Double valor) {
-		return Utils.FormatandoNumeros.format(valor);
-	}
+    public static String doubleToString(Double valor) {
+        return Utils.formatandoNumeros.format(valor);
+    }
 }
